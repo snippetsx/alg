c56d7016424f0cedfe534f8a43b80c081d9aaf33
SnippetsX <snippetsx@SnippetsXs-iMac.local>
2023-09-02 17:24:16 +0300

    Added push back

--- list.c
+++ list.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <list.h>
 
+
+size_t list_size(const t_list *head)
+{
+    size_t cnt = 0;
+
+    while (head)
+    {
+        cnt++;
+        head = head->next;
+    }
+    return cnt;
+}
+
 void list_push(t_list **head, int x)
 {
     t_list *tmp;
@@ -18,7 +31,7 @@ int list_pop(t_list **head)
     t_list* prev = NULL;
     int ret;
 
-    if (head == NULL)
+    if (head == NULL || *head == NULL)
     {
         return -1;
     }
@@ -32,8 +45,8 @@ int list_pop(t_list **head)
 void list_insert(t_list *head, unsigned n, int val)
 {
     unsigned i = 0;
-    
     t_list *tmp;
+
     while (i < n && head->next)
     {
         head = head->next;
@@ -54,6 +67,8 @@ void list_insert(t_list *head, unsigned n, int val)
 
 void list_remove(t_list **head)
 {
+    if (!head || !*head)
+        return ;
     while ((*head)->next)
     {
         list_pop(head);
@@ -62,14 +77,33 @@ void list_remove(t_list **head)
     free(*head);
 }
 
-void list_print(const t_list *head)
+void list_foreach(const t_list *head, void (*f)(int))
 {
+    if (!f)
+        return ;
     while (head)
     {
-        printf("%d ", head->val);
+        f(head->val);
         head = head->next;
     }
-    printf("\n");
 }
 
+void list_push_back(t_list** head, int val)
+{
+    t_list* temp = (t_list*)malloc(sizeof(t_list));
+    t_list* iterator = *head;
 
+    temp->val = val;
+    temp->next = NULL;
+
+    if (iterator == NULL)
+    {
+        *head = temp;
+        return;
+    }
+    while (iterator->next != NULL)
+    {
+        iterator = iterator->next;
+    }
+    iterator->next = temp;
+}
--- list.h
+++ list.h
@@ -3,6 +3,8 @@ typedef struct s_list {
   struct s_list* next;
 } t_list;
 
+size_t list_size(const t_list *head);
+
 void list_push(t_list **head, int x);
 
 int list_pop(t_list **head);
@@ -11,4 +13,7 @@ void list_insert(t_list *head, unsigned n, int val);
 
 void list_remove(t_list **head);
 
-void list_print(const t_list *head);
+void list_foreach(const t_list *head, void (*f)(int));
+
+void list_push_back(t_list** head, int val);
+
