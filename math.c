d40f015e677994cf1a82caf399ab9f7bb76a3afa
SnippetsX <snippetsx@SnippetsXs-iMac.local>
2023-09-24 23:38:31 +0300

    added header file and binpow gcd and gray alg's

--- math.c
+++ math.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 int is_prime(unsigned long long value)
 {
     
@@ -16,6 +18,24 @@ int gcd(int a, int b)
 		return a;
 	else
 		return gcd (b, a % b);
+}
+
+int binpow(int a, int n) 
+{
+	if (n == 0)
+		return 1;
+	if (n % 2 == 1)
+		return binpow(a, n - 1) * a;
+	else
+    {
+		int b = binpow(a, n / 2);
+		return b * b;
+	}
+}
+
+int gray(int n) 
+{
+	return n ^ (n >> 1);
 }
 
 
