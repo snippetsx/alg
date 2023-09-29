f412361b23c5e7477dcacae25a40f664ab3a74da
SnippetsX <snippetsx@SnippetsXs-iMac.local>
2023-09-29 15:26:37 +0300

    resolves from review

--- b_tree.c
+++ b_tree.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <b_tree.h>
 
-// stdin
-// stdout
-// stderr
-
-// Передача параметров в функцию
-// 1. По значению
-// 2. По ссылке
-
-
 btree_t *b_new_node(int item) {
     btree_t *temp = (btree_t *)malloc(sizeof(btree_t));
     if(!temp)
@@ -29,32 +20,6 @@ btree_t *b_new_node(int item) {
 //         b_print(root->right);
 //     }
 // }
-
-btree_t *queue[100];
-int head = 0;
-int tail = 0;
-void bfs(btree_t *root)
-{
-    queue[0] = root;
-    ++tail;
-
-    while (head != tail)
-    {
-        if (queue[head])
-        {
-            queue[tail++] = queue[head]->left;
-            queue[tail++] = queue[head]->right;
-            
-            printf("%p\n"
-                   "______________________\n"
-                   "| %18d |\n"
-                   "| %18p |\n"
-                   "| %18p |\n"
-                   "______________________\n\n\n", queue[head], queue[head]->key, queue[head]->left, queue[head]->right);
-        }
-        ++head;
-    }
-}
 
 void b_insert(btree_t **root, int key)
 {
@@ -133,12 +98,10 @@ int b_search(btree_t **root, int value) //Почти бинпоиск. Если 
         if(walker->key < value)
         {
             walker = walker->left;
-            continue;
         }
-        else if (walker->key > value)
+        if (walker->key > value)
         {
             walker = walker->left;
-            continue;
         }
         else{
             return 1;
